9e194cffc4f6a0e88cb77fb36155af2482a9adab
Akriti Tamrakar <akritit009@gmail.com>
2026-08-02 00:29:43 +0530

    Use environment variables and remove pycache

--- backend/sql_connection.py
+++ backend/sql_connection.py
@@ -1,8 +1,12 @@
 import mysql.connector
 import os
+from dotenv import load_dotenv
+
+from pathlib import Path
+
+load_dotenv(Path(__file__).resolve().parent.parent / ".env")
 
 __cnx = None
-
 
 def get_sql_connection():
     global __cnx
@@ -10,13 +14,18 @@ def get_sql_connection():
     if __cnx is None:
         print("Opening MySQL connection")
 
+        print("HOST:", os.getenv("DB_HOST"))
+        print("USER:", os.getenv("DB_USER"))
+        print("DB:", os.getenv("DB_NAME"))
+        print("PORT:", os.getenv("DB_PORT"))
+
         __cnx = mysql.connector.connect(
-            host="mysql-38021bf5-akritit009-ceb2.k.aivencloud.com",
-            user="avnadmin",
-            password="REMOVED",
-            database="grocery_store",
-            port=19567,
-            ssl_ca=r"C:\Users\akrit\Downloads\ca.pem"
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            port=int(os.getenv("DB_PORT")),
+            ssl_ca=os.getenv("SSL_CA")
         )
 
         print("MySQL Connected Successfully")
