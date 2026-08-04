14ef22fd8bd1cefca4a6a527fe1933d57dab32de
Akriti Tamrakar <akritit009@gmail.com>
2026-08-05 00:01:33 +0530

    Fix order page

--- backend/static/js/custom/common.js
+++ backend/static/js/custom/common.js
@@ -27,13 +27,19 @@ function callApi(method, url, data) {
 }
 function calculateValue() {
     var total = 0;
-    $(".product-item").each(function( index ) {
-        var qty = parseFloat($(this).find('.product-qty').val());
-        var price = parseFloat($(this).find('#product_price').val());
-        price = price*qty;
-        $(this).find('#item_total').val(price.toFixed(2));
-        total += price;
+
+    $(".product-item").each(function () {
+
+        var qty = parseFloat($(this).find(".product-qty").val()) || 0;
+        var price = parseFloat($(this).find(".product-price").val()) || 0;
+
+        var itemTotal = qty * price;
+
+        $(this).find(".product-total").val(itemTotal.toFixed(2));
+
+        total += itemTotal;
     });
+
     $("#product_grand_total").val(total.toFixed(2));
 }
 
--- backend/static/js/custom/order.js
+++ backend/static/js/custom/order.js
@@ -1,157 +1,162 @@
 var productPrices = {};
 
-$(function () {
-    //Json data by api call for order table
-  $.get(productListApiUrl)
-.done(function(response) {
-    console.log("Products:", response);
+$(document).ready(function () {
 
-    productPrices = {};
+    // Load products
+    $.get(productListApiUrl)
+        .done(function (response) {
 
-    var options = '<option value="">--Select--</option>';
+            console.log("Products:", response);
 
-    $.each(response, function(index, product) {
-        options += `<option value="${product.product_id}">
-                        ${product.name}
-                    </option>`;
+            productPrices = {};
 
-        productPrices[product.product_id] = product.price_per_unit;
+            var options = '<option value="">--Select--</option>';
+
+            $.each(response, function (index, product) {
+
+                options += '<option value="' + product.product_id + '">' +
+                    product.name +
+                    '</option>';
+
+                productPrices[product.product_id] = product.price_per_unit;
+            });
+
+            $(".product-box select").html(options);
+        })
+        .fail(function (xhr) {
+            console.log(xhr);
+            alert("Cannot connect to backend");
+        });
+
+
+    // Add More Button
+    $("#addMoreButton").click(function () {
+
+        var row = $(".product-box").html();
+
+        $("#itemsInOrder").append(row);
+
+        calculateValue();
     });
 
-    $(".product-box").find("select").html(options);
-})
-.fail(function(xhr) {
-    console.log(xhr);
-    alert("Cannot connect to backend");
-});
-$("#addMoreButton").click(function () {
-    var row = $(".product-box").html();
-    $(".product-box-extra").append(row);
-    $(".product-box-extra .remove-row").last().removeClass('hideit');
-    $(".product-box-extra .product-price").last().val('0.0');
-    $(".product-box-extra .product-qty").last().val('1');
-    $(".product-box-extra .product-total").last().val('0.0');
-});
 
-$(document).on("click", ".remove-row", function (){
-    $(this).closest('.row').remove();
-    calculateValue();
-});
-
-$(document).on("change", ".cart-product", function (){
-    var product_id = $(this).val();
-    var price = productPrices[product_id];
-
-    $(this).closest('.row').find('#product_price').val(price);
-    calculateValue();
-});
-
-$(document).on("change", ".product-qty", function (e){
-    calculateValue();
-});
-
-$("#saveOrder").on("click", function () {
-
-    var formData = $("form").serializeArray();
-
-    var requestPayload = {
-        customer_name: "",
-        grand_total: 0,
-        order_details: []
-    };
+    // Remove Row
+    $(document).on("click", ".remove-row", function () {
+        $(this).closest(".product-item").remove();
+        calculateValue();
+    });
 
 
-    for (var i = 0; i < formData.length; i++) {
+    // Product Change
+    $(document).on("change", ".cart-product", function () {
 
-        var element = formData[i];
+        var product_id = $(this).val();
+        var price = productPrices[product_id] || 0;
 
-        switch(element.name) {
+        $(this)
+            .closest(".product-item")
+            .find(".product-price")
+            .val(price);
 
-            case "customerName":
-                requestPayload.customer_name = element.value;
-                break;
-
-
-            case "product_grand_total":
-                requestPayload.grand_total = element.value;
-                break;
-
-
-            case "product":
-
-                if(element.value != "") {
-
-                    requestPayload.order_details.push({
-                        product_id: element.value,
-                        quantity: 0,
-                        total_price: 0
-                    });
-
-                }
-
-                break;
-
-
-            case "qty":
-
-                if(requestPayload.order_details.length > 0){
-
-                    requestPayload.order_details[
-                        requestPayload.order_details.length - 1
-                    ].quantity = element.value;
-
-                }
-
-                break;
-
-
-            case "item_total":
-
-                if(requestPayload.order_details.length > 0){
-
-                    requestPayload.order_details[
-                        requestPayload.order_details.length - 1
-                    ].total_price = element.value;
-
-                }
-
-                break;
-        }
-    }
-
-
-    alert(JSON.stringify(requestPayload, null, 2));
-
-
-    $.ajax({
-
-        url: orderSaveApiUrl,
-
-        type: "POST",
-
-        data: {
-            data: JSON.stringify(requestPayload)
-        },
-
-
-        success:function(response){
-
-            alert("Order Saved Successfully");
-
-            console.log(response);
-
-        },
-
-
-        error:function(xhr){
-
-            console.log(xhr.responseText);
-
-            alert("Error while saving order");
-
-        }
+        calculateValue();
 
     });
 
+
+    // Quantity Change
+    $(document).on("keyup change", ".product-qty", function () {
+        calculateValue();
+    });
+
+
+    // Save Order
+    $("#saveOrder").click(function () {
+
+        var formData = $("form").serializeArray();
+
+        var requestPayload = {
+            customer_name: "",
+            grand_total: 0,
+            order_details: []
+        };
+
+        for (var i = 0; i < formData.length; i++) {
+
+            var element = formData[i];
+
+            switch (element.name) {
+
+                case "customerName":
+                    requestPayload.customer_name = element.value;
+                    break;
+
+                case "product_grand_total":
+                    requestPayload.grand_total = element.value;
+                    break;
+
+                case "product":
+
+                    if (element.value !== "") {
+
+                        requestPayload.order_details.push({
+                            product_id: element.value,
+                            quantity: 0,
+                            total_price: 0
+                        });
+
+                    }
+
+                    break;
+
+                case "qty":
+
+                    if (requestPayload.order_details.length > 0) {
+
+                        requestPayload.order_details[
+                            requestPayload.order_details.length - 1
+                        ].quantity = element.value;
+
+                    }
+
+                    break;
+
+                case "item_total":
+
+                    if (requestPayload.order_details.length > 0) {
+
+                        requestPayload.order_details[
+                            requestPayload.order_details.length - 1
+                        ].total_price = element.value;
+
+                    }
+
+                    break;
+            }
+        }
+
+        $.ajax({
+
+            url: orderSaveApiUrl,
+
+            type: "POST",
+
+            data: {
+                data: JSON.stringify(requestPayload)
+            },
+
+            success: function (response) {
+                alert("Order Saved Successfully");
+                console.log(response);
+                location.reload();
+            },
+
+            error: function (xhr) {
+                console.log(xhr.responseText);
+                alert("Error while saving order");
+            }
+
+        });
+
+    });
 
 });
